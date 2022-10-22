83084f0570bd0dd49156cff5d6171450d500cd24
carbonhrz <77118268+carbonhrz@users.noreply.github.com>
2022-10-22 17:43:41 +0200

    Delete FacultyTest.java

--- FacultyTest.java
+++
@@ -1,8 +0,0 @@
-public class FacultyTest { 
-
-    public static void main(String[] args) {
-        System.out.println("Gib die zu untersuchende Zahl ein: ");
-        int number = new java.util.Scanner(System.in).nextInt();
-    }
-    
-}
